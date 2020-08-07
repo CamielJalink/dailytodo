255cfa7cb8b5e1c0f2d621f2e6c09bf4a0401430
Camiel <camiel.jalink@gmail.com>
2020-08-07 10:22:43 +0200

    Port as a seperate variable. Should this really be a Number instead of a string?

--- serverside/index.js
+++ serverside/index.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
 var app = express_1.default();
+var port = Number(process.env.PORT) || 9000;
 app.use(express_1.default.static(path_1.default.join('../clientside/build')));
 app.get('/', function (req, res) {
     res.sendFile(path_1.default.join('../clientside/build', 'index.html'));
 });
-app.listen(9000, function () {
+app.listen(port, process.env.IP || '', function () {
     console.log("Server is running");
 });
--- serverside/index.ts
+++ serverside/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import path from "path";
 
 const app = express();
+let port = Number(process.env.PORT) || 9000;
 app.use(express.static(path.join('../clientside/build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join('../clientside/build', 'index.html'));
 });
 
-app.listen(Number(process.env.PORT) || 9000, process.env.IP || '', () => {
+app.listen(port, process.env.IP || '', () => {
   console.log("Server is running");
 });
